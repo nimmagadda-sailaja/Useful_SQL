6dea906272c39491ff6a01e7f8e277ee9f151c42
snelluri <sivaram.nelluri@gmail.com>
2018-09-29 12:02:31 -0500

    Update Top10StoresIntheQuarterSql.sql

--- Top10StoresIntheQuarterSql.sql
+++ Top10StoresIntheQuarterSql.sql
@@ -7,24 +7,24 @@ SELECT storeid,
   (
      SELECT 
 	       storeid, 
-		   sakes_date (FORMAT 'YYYY-MM') (CHAR(7)) yr_mnth, 
+		   sales_date (FORMAT 'YYYY-MM') (CHAR(7)) yr_mnth, 
 		   sum(sales_amt) total_sales
     FROM sales
-    WHERE sakes_date BETWEEN add_months(current_date, -3) 
+    WHERE sales_date BETWEEN add_months(current_date, -3) 
 	  AND current_date 
     GROUP BY 1,2
    )a
    QUALIFY RANK() OVER(PARTITION BY yr_mnth ORDER BY total_sales desc) <=10
  
- /* Without using the ordered analytical functions */
+ /* Without using the ordered analytical functions but using correlated subquery*/
 
  WITH tmp_sales
   AS (
        SELECT storeid, 
-	          sakes_date (FORMAT 'YYYY-MM') (CHAR(7)) yr_mnth, 
+	          sales_date (FORMAT 'YYYY-MM') (CHAR(7)) yr_mnth, 
 			  sum(sales_amt) total_sales
          FROM sales a
-         WHERE sakes_date BETWEEN ADD_MONTHS(current_date, -3) 
+         WHERE sales_date BETWEEN ADD_MONTHS(current_date, -3) 
 		   AND current_date 
          GROUP BY 1,2
  )
@@ -35,3 +35,24 @@ SELECT storeid,
                WHERE a.yr_mnth = b.yr_mnth
 			     AND a.total_sales < b.total_sales
 				)
+;
+/* Without corrleated subquery and rank functions */
+WITH total_by_mth AS
+(SELECT storeid, 
+        sales_date (FORMAT 'YYYY-MM') (CHAR(7)) year_mth,
+        SUM(sales_amt) total_sales
+   FROM sales
+  WHERE sales_date BETWEEN ADD_MONTHS(current_date, -3) 
+		   AND current_date
+GROUP BY 1,2
+)
+SELECT a.storeid, 
+       a.year_mth, 
+	   a.total_sales, 
+	   count( case when a.total_sales <= b.total_sales then a.sls_tran_loc_id end) as rnk
+  FROM total_by_mth a
+       join total_by_mth b
+    ON a.year_mth = b.year_mth 
+	
+GROUP BY 1,2,3
+HAVING rnk <=10
